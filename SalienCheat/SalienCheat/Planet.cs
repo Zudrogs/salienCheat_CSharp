14e849676fd7e419ba43d70c2d52a2b475f09bc8
Sofus Nielsen <sofushn@hotmail.dk>
2018-06-28 01:09:07 +0200

    Added alot of base functionality, and started on the main script

---
+++ SalienCheat/SalienCheat/Planet.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalienCheat
+{
+    public class Rootobject
+    {
+        public Response response { get; set; }
+    }
+
+    public class Response
+    {
+        public string active_planet { get; set; }
+        public int time_on_planet { get; set; }
+        public string active_zone_game { get; set; }
+        public string active_zone_position { get; set; }
+        public int time_in_zone { get; set; }
+        public Clan_Info clan_info { get; set; }
+        public string score { get; set; }
+        public int level { get; set; }
+        public string next_level_score { get; set; }
+
+        public Planet[] planets { get; set; }
+    }
+
+    public class Clan_Info
+    {
+        public int accountid { get; set; }
+        public string name { get; set; }
+        public string avatar { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Planet :IComparable<Planet>
+    {
+        public string id { get; set; }
+        public State state { get; set; }
+        public Zone[] zones { get; set; }
+
+        public int CompareTo(Planet other)
+        {
+            if(other == null)
+                return 1;
+
+            return id.CompareTo(other.id);
+        }
+    }
+
+    public class State
+    {
+        public string name { get; set; }
+        public int difficulty { get; set; }
+        public bool active { get; set; }
+        public int activation_time { get; set; }
+        public int position { get; set; }
+        public bool captured { get; set; }
+        public float capture_progress { get; set; }
+        public int total_joins { get; set; }
+        public int current_players { get; set; }
+        public int priority { get; set; }
+        public string tag_ids { get; set; }
+    }
+
+    public class Zone :IComparable<Zone>
+    {
+        public int zone_position { get; set; }
+        public int type { get; set; }
+        public string gameid { get; set; }
+        public int difficulty { get; set; }
+        public bool captured { get; set; }
+        public float capture_progress { get; set; }
+
+        public int CompareTo(Zone other)
+        {
+            if(other == null)
+                return 1;
+
+            return difficulty.CompareTo(other.difficulty);
+        }
+
+        public static bool operator >(Zone operand1, Zone operand2)
+        {
+            return operand1.CompareTo(operand2) == 1;
+        }
+        public static bool operator <(Zone operand1, Zone operand2)
+        {
+            return operand1.CompareTo(operand2) == -1;
+        }
+        public static bool operator >=(Zone operand1, Zone operand2)
+        {
+            return operand1.CompareTo(operand2) >= 0;
+        }
+        public static bool operator <=(Zone operand1, Zone operand2)
+        {
+            return operand1.CompareTo(operand2) <= 0;
+        }
+    }
+
+    public class Result
+    {
+        public int HighZones { get; set; }
+        public int MediumZones { get; set; }
+        public int LowZones { get; set; }
+        public List<string> ZoneMessages { get; set; }
+        public Planet BestPlanet { get; set; }
+        public Zone BestZone { get; set; }
+    }
+}
--- SalienCheat/SalienCheat/Program.cs
+++ SalienCheat/SalienCheat/Program.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Net.Http.Formatting;
 
 namespace SalienCheat
 {
@@ -8,8 +14,6 @@ namespace SalienCheat
     {
         const int WaitTime = 110;
         static string Token;
-        static List<string> KnownPlanets = new List<string>();
-        static List<string> SkippedPlanets = new List<string>();
         static List<string> ZonePaces = new List<string>();
 
         //TODO Add suport for external token file
@@ -17,48 +21,206 @@ namespace SalienCheat
         {
             StartUpMessage();
 
-            bool BestPlanetAndZone;
+            Result BestPlanetAndZone;
+            do
+            {
+                BestPlanetAndZone = GetBestPlanetAndZone();
+                if(BestPlanetAndZone == null)
+                    Thread.Sleep(5000);
+            } while(BestPlanetAndZone == null);
+
+            do
+            {
+                Console.WriteLine();
+
+                string steamThinksPlanet;
+                do
+                {
+                    steamThinksPlanet = LeaveCurrentGame(BestPlanetAndZone.BestPlanet);
+                    if(BestPlanetAndZone.BestPlanet.id != steamThinksPlanet)
+                    {
+                        ExecuteRequest("ITerritoryControlMinigameService/JoinPlanet", GetOrPost.Post, $"id={BestPlanetAndZone.BestPlanet.id}&access_token={Token}");
+                        steamThinksPlanet = LeaveCurrentGame();
+                    }
+                } while(steamThinksPlanet != BestPlanetAndZone.BestPlanet.id);
+
+
+
+                Console.WriteLine("Zone: {0} on Planet: {1}", BestPlanetAndZone.BestZone.zone_position, BestPlanetAndZone.BestPlanet.id);
+
+
+                Thread.Sleep(30000);
+            } while(true);
+        }
+
+        static string LeaveCurrentGame(Planet bestPlanet = null)
+        {
+            Response playerData = null;
+            do
+            {
+                playerData = ExecuteRequest("ITerritoryControlMinigameService/GetPlayerInfo", GetOrPost.Post, "access_token=" + Token).response;
+                if(playerData.active_zone_game != null)
+                    ExecuteRequest("IMiniGameService/LeaveGame", GetOrPost.Post, $"access_token={Token}&gameid={playerData.active_zone_game}");
+            } while(playerData != null);
+
+            string activePlanet = playerData.active_planet;
+
+            if(bestPlanet.id != activePlanet && bestPlanet != null)
+            {
+                PrintWithColor($"   Leaving planet |]g{activePlanet}|]n because we want to be on |]g{bestPlanet.id} ({bestPlanet.state.name})");
+                PrintWithColor($"   Time accumulated on planet |]g{activePlanet}|]n: |]y{playerData.time_on_planet}");
+                Console.WriteLine();
+
+                ExecuteRequest("IMiniGameService/LeaveGame", GetOrPost.Post, $"access_token={Token}&gameid={activePlanet}");
+            }
+
+            return playerData.active_planet;
+        }
+
+        static Result GetBestPlanetAndZone()
+        {
+            Planet[] planets = ExecuteRequest("ITerritoryControlMinigameService/GetPlanets", GetOrPost.Get, "active_only=1&language=english").response.planets;
+
+            if(planets.Length == 0 || planets == null)
+                return null;
+
+            Result planetResult = null;
+            int sortKey = 0;
+
+            foreach(Planet p in planets)
+            {
+                Result zoneResult = null;
+                do
+                {
+                    zoneResult = GetPlanetState(p.id);
+                } while(zoneResult == null);
+
+                //Set the planet result to the same as zone result
+                planetResult = zoneResult;
+
+                PrintWithColor(string.Format(">> Planet |]g{0}|]n - Captured: |]g{1:p}|]n - High: |]y{2}|]n - Medium: |]y{3}|]n - Low: |]y{4}|]n - Players: |]y{5:n0} |]g({6})",
+                                            p.id, p.state.capture_progress, planetResult.HighZones, planetResult.MediumZones, planetResult.LowZones, p.state.current_players, p.state.name));
+
+                int localSortKey = 0;
+
+                if(planetResult.LowZones > 0)
+                    localSortKey =+ 99 - planetResult.LowZones;
+                if(planetResult.MediumZones > 0)
+                    localSortKey =+ (int)Math.Pow(10, 2) * (99 - planetResult.MediumZones);
+                if(planetResult.HighZones > 0)
+                    localSortKey =+ (int)Math.Pow(10, 4) * (99 - planetResult.HighZones);
+
+                if(localSortKey >= sortKey)
+                    zoneResult.BestPlanet = p;
+            }
+
+            //PrintWithColor(string.Format(">> Next Zone is |]y{0}|]n (Captured: |]y{1:F2}|]n - Difficulty: |]y{2}|]n) on Planet |]g{3} ({4})"));
+
+
+            return planetResult;
+        }
+        static Result GetPlanetState(string planetId)
+        {
+            string filter = "id=" + planetId + "&language=english";
+            Zone[] zones = ExecuteRequest("ITerritoryControlMinigameService/GetPlanet", GetOrPost.Get, filter).response.planets[0].zones;
+
+            if(zones.Length == 0 || zones == null)
+            {
+                return null;
+            }
+
+            Result zoneResult = new Result();
+            //int zonePace;
+            
+            foreach(Zone z in zones)
+            {
+                if(z.captured)
+                    continue;
+
+                float cutoff = 0.99F;
+
+                //TODO calculate/check zone pace HERE
+                // '     Zone {yellow}%3d{normal} - Captured: {yellow}%5s%%{normal} - Cutoff: {yellow}%5s%%{normal} - Pace: {yellow}%6s%%{normal} - ETA: {yellow}%2dm %2ds{normal}',
+
+                if(z.capture_progress >= cutoff)
+                    continue;
+
+                //Check for grater or equal: makes it so it always sets the "last and best" zone
+                if(z >= zoneResult.BestZone)
+                    zoneResult.BestZone = z;
+
+                //Count number of each zone difficulty still active
+                switch(z.difficulty)
+                {
+                    case 3: zoneResult.HighZones++; break;
+                    case 2: zoneResult.MediumZones++; break;
+                    case 1: zoneResult.LowZones++; break; 
+                }
+            }
             
 
-
-
-#if DEBUG
-            Console.ReadKey();
-#endif
+            return zoneResult;
         }
 
-
-
-
-        static bool GetBestPlanetAndZone()
-        {
-            string[] Planets = SendGet("ITerritoryControlMinigameService/GetPlanets", "active_only=1&language=english");
-
-
-            return true;
-        }
-
-        static void SendPost(string method, string data)
-        {
-            ExecuteRequest(method, "https://community.steam-api.com/" + method + "/v0001/?", data);
-        }
-        static string[] SendGet(string method, string data)
-        {
-            ExecuteRequest(method, "https://community.steam-api.com/" + method + "/v0001/", data);
-            return null;
-        }
-
+        //Gets the HttpClient
         static HttpClient GetHttpClient()
         {
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://steamcommunity.com/saliengame/play");
-            client.Timeout = new TimeSpan(3000);
+            client.BaseAddress = new Uri("https://community.steam-api.com/");
+            client.Timeout = new TimeSpan(0, 0, 30);
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3464.0 Safari/537.36");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("Application/json"));
+
+            client.DefaultRequestHeaders.Add("Accept", "*/*");
+            client.DefaultRequestHeaders.Add("Origin", "https://steamcommunity.com");
+            client.DefaultRequestHeaders.Add("Referer", "https://steamcommunity.com/saliengame/play");
+            client.DefaultRequestHeaders.Add("Connection", "Keep-Alive");
+            client.DefaultRequestHeaders.Add("Keep-Alive", "300");
+
             return client;
         }
 
-        static void ExecuteRequest(string method, string url, string data)
+        static Rootobject ExecuteRequest(string method, GetOrPost getOrPost, string filterOrContent = "")
         {
+            
+            
+            using(HttpClient client = GetHttpClient())
+            {
+                string url = "";
+                HttpResponseMessage result = null;
 
+                switch(getOrPost)
+                {
+                    case GetOrPost.Get:
+                        url = method + "/v0001/?" + filterOrContent;
+                        result = client.GetAsync(url).Result;
+                        break;
+                    case GetOrPost.Post:
+                        url = method + "/v0001/";
+
+                        List<KeyValuePair<string, string>> contentList = new List<KeyValuePair<string, string>>();
+                        foreach(string s in filterOrContent.Split('&'))
+                        {
+                            string[] tmpSplit = s.Split('=');
+                            contentList.Add(new KeyValuePair<string, string>(tmpSplit[0], tmpSplit[1]));
+                        }
+
+                        FormUrlEncodedContent content = new FormUrlEncodedContent(contentList);
+                        result = client.PostAsync(url, content).Result;
+                        break;
+                }
+
+                //TODO Error handling
+                if(!result.IsSuccessStatusCode)
+                {
+                    
+
+                    //PrintWithColor(string.Format("|[r!! {0} failed - Error:  - {1}", method, filter));
+                }
+
+                return result.Content.ReadAsAsync<Rootobject>().Result;
+
+            } 
         }
 
 
@@ -73,10 +235,11 @@ namespace SalienCheat
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.WriteLine("Welcome to SalienCheat for SteamDB");
             Console.BackgroundColor = ConsoleColor.Black;
-
-            PrintWithColor("|]g-- If you want to support us, join our group");
+            PrintWithColor("|]g-- This script is based on SteamDB's PhP script");
+            PrintWithColor("|]g--|]y https://github.com/SteamDatabase/SalienCheat");
+            PrintWithColor("|]g-- If you want to support them, join there group");
             PrintWithColor("|]g--|]y https://steamcommunity.com/groups/steamdb");
-            PrintWithColor("|]g-- and set us as your clan on");
+            PrintWithColor("|]g-- and set them as your clan on");
             PrintWithColor("|]g--|]y https://steamcommunity.com/saliengame/play");
             PrintWithColor("|]g-- Happy farming!");
         }
@@ -94,9 +257,14 @@ namespace SalienCheat
 
             foreach(string s in messageParts)
             {
-                if(String.IsNullOrWhiteSpace(s))
+                if(!s.StartsWith(']'))
+                {
+                    Console.Write(s);
                     continue;
-                if(s.StartsWith("]n")) 
+                }
+                else if(String.IsNullOrWhiteSpace(s))
+                    continue;
+                else if(s.StartsWith("]n")) 
                     Console.ForegroundColor = ConsoleColor.Gray;
                 else if(s.StartsWith("]g")) 
                     Console.ForegroundColor = ConsoleColor.DarkGreen;
@@ -104,10 +272,12 @@ namespace SalienCheat
                     Console.ForegroundColor = ConsoleColor.Yellow;
                 else if(s.StartsWith("]r"))
                     Console.ForegroundColor = ConsoleColor.Red;
-                
+
                 Console.Write(s.Remove(0, 2));
             }
             Console.WriteLine();
         }
     }
+    enum GetOrPost { Get, Post }
+    enum Difficulty { Low = 1, Medium = 2, High = 3 }
 }
